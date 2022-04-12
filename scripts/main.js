c7fc49bda61f2ae96ae335234d3ced610232f715
aja-lunavista <100550338+aja-lunavista@users.noreply.github.com>
2022-04-12 19:38:08 -0400

    Delete scripts directory

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-function upDate(previewPic){
-    document.getElementById('image').innerHTML=previewPic.alt;
-    document.getElementById('image').style.backgroundImage="url('"+previewPic.src+"')";
-}
-
-function unDo(){
-    document.getElementById('image').style.backgroundImage="url('')";
-    document.getElementById('image').innerHTML="FAV STYLES - Hover over a thumbnail.";
-}
